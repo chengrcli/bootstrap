d1d889864dcd1bbd6ca5bdd26863112e50f04ba5
chengli3 <cheng1.li@intel.com>
2019-10-26 15:08:56 +0800

    Extract the files which can be shared

--- bootstrap18.sh
+++ bootstrap18.sh
@@ -1,9 +1,8 @@
 systemctl set-default multi-user.target
 systemctl stop systemd-resolved.service
 systemctl disable systemd-resolved.service
-sed -i 's/4.2.2.1/10.248.2.5/' /etc/netplan/01-netcfg.yaml
-sed -i 's/4.2.2.2/10.239.27.228/' /etc/netplan/01-netcfg.yaml
-sed -i 's/, 208.67.220.220//' /etc/netplan/01-netcfg.yaml
+sed -i '/nameservers/d' /etc/netplan/01-netcfg.yaml
+sed -i '/addresses/d' /etc/netplan/01-netcfg.yaml
 parted -a optimal /dev/sda ---pretend-input-tty resizepart 3 yes 100%
 resize2fs /dev/sda3
 rm -f /etc/resolv.conf && cp -r /vagrant/etc /
---
+++ setup_vagrant.sh
@@ -0,0 +1,4 @@
+sudo apt update
+sudo apt install -y libvirt-bin qemu dnsmasq
+sudo apt install vagrant
+vagrant plugin install vagrant-libvirt
