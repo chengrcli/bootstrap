3a7ac355eec9e5a729c34655a9c675f7fcdb7847
cheng <chengtcli@qq.com>
2022-01-29 12:44:57 +0800

    share: generate ssh local instead of using a fixed one
    Signed-off-by: cheng <chengtcli@qq.com>

--- setup_vagrant.sh
+++ setup_vagrant.sh
@@ -7,7 +7,10 @@ then
 elif which yum&>/dev/null
 then
   sudo yum install -y libvirt libvirt-devel ruby-devel gcc qemu-kvm
-  curl -LO https://releases.hashicorp.com/vagrant/2.2.13/vagrant_2.2.13_x86_64.rpm
+  if [ ! -e vagrant_2.2.13_x86_64.rpm ]
+  then
+    curl -LO https://releases.hashicorp.com/vagrant/2.2.13/vagrant_2.2.13_x86_64.rpm
+  fi
   sudo yum install -y vagrant_2.2.13_x86_64.rpm
   sudo systemctl start libvirtd
 fi
@@ -16,3 +19,12 @@ fi
 sudo usermod -a -G libvirt $USER
 vagrant plugin install vagrant-libvirt
 
+# generate ssh key for VM access each other
+mkdir -p shares/ssh
+pushd shares/ssh
+  if [ ! -e id_rsa ]
+  then
+    ssh-keygen -C "for vm access each other" -f id_rsa -N ''
+    cp id_rsa.pub authorized_keys
+  fi
+popd
